abace081f877ad2eb1680073fb8f2e76e70892b4
ibrahim <ibrahimsabaa244@gmail.com>
2025-11-01 21:27:17 +0200

    Add ApplicationUser and ApplicationRole Enities And Configurations

---
+++ Back-end/Sehaty.Soluation/Sehaty.Infrastructure/Data/Configrations/ApplicationRoleConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sehaty.Core.Entities.User_Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sehaty.Infrastructure.Data.Configrations
+{
+    public class ApplicationRoleConfiguration : IEntityTypeConfiguration<ApplicationRole>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationRole> builder)
+        {
+            builder.ToTable("Roles");
+            builder.Property(n => n.Name)
+                .HasColumnType("nvarchar")
+                .HasMaxLength(50)
+                .IsRequired();
+            builder.HasIndex(n => n.Name).IsUnique();
+            builder.Property(n => n.Description)
+                .HasColumnType("nvarchar(max)");
+        }
+    }
+}
