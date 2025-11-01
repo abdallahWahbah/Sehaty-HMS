3378d91ce272fbd479d0706350226251280d4799
Ahmed Abdulrahim <hemo089@outlook.com>
2025-11-01 17:05:57 +0200

    Create-MedicalRecord-&&-DoctorAvailability

--- Back-end/Sehaty.Soluation/Sehaty.Core/Entities/Business_Entities/DoctorAvailabilitySlot.cs
+++ Back-end/Sehaty.Soluation/Sehaty.Core/Entities/Business_Entities/DoctorAvailabilitySlot.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,17 @@ namespace Sehaty.Core.Entites
 {
     public class DoctorAvailabilitySlot : BaseEntity
     {
+        [ForeignKey(nameof(Doctors))]
+        [Column(TypeName = "nvarchar(10)")]
+        public string Doctor_ID { get; set; }
+        public Doctor Doctors { get; set; }
+        [Column(TypeName = "nvarchar(10)")]
+
+        public string? Day_of_Week { get; set; }
+        public TimeOnly Start_Time { get; set; }
+        public TimeOnly End_Time { get; set; }
+        public bool Is_Recurring { get; set; }
+        public DateOnly? Date { get; set; }
+        public bool Available_Flag { get; set; }
     }
 }
