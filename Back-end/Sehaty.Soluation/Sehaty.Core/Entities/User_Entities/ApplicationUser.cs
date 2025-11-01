fab34c91795953bfbe79aac95505e5fb2c4455d7
hanaa-elagmi <hanaa.elagmi@gmail.com>
2025-11-01 18:21:10 +0200

    Added notification feature

--- Back-end/Sehaty.Soluation/Sehaty.Core/Entities/User_Entities/ApplicationUser.cs
+++ Back-end/Sehaty.Soluation/Sehaty.Core/Entities/User_Entities/ApplicationUser.cs
@@ -9,6 +9,7 @@ namespace Sehaty.Core.Entities.User_Entities
 {
     public class ApplicationUser : BaseEntity
     {
-        
-    }
+		public ICollection<Notification> Notification { get; set; }=new List<Notification>();
+
+	}
 }
---
+++ Back-end/Sehaty.Soluation/Sehaty.Infrastructure/Data/Configrations/NotificationConfigrations.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sehaty.Core.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sehaty.Infrastructure.Data.Configrations
+{
+	public class NotificationConfigrations : IEntityTypeConfiguration<Notification>
+	{
+		public void Configure(EntityTypeBuilder<Notification> builder)
+		{
+			builder.ToTable("Notifications");  //table name
+			builder.HasKey(n => n.Id); //primary key
+			builder.Property(n => n.Id).ValueGeneratedOnAdd(); //identity
+			builder.Property(n => n.Title).IsRequired().HasMaxLength(200);
+			builder.Property(n => n.Message).IsRequired();
+			builder.Property(n => n.CreatedAt).IsRequired();
+			builder.Property(n => n.CreatedAt)
+				   .HasDefaultValueSql("GETDATE()");
+			builder.HasOne(n => n.User)
+				   .WithMany(u => u.Notification)
+				   .HasForeignKey(n => n.UserId)
+				   .OnDelete(DeleteBehavior.Cascade);
+			builder.Property(n => n.NotificationType)
+				   .HasConversion<string>()  
+				   .IsRequired();
+
+		}
+	}
+}
