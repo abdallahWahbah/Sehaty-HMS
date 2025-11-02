98139118e1a54e93e2ea8b6efccafdbd43ad20e5
Abdallah Wahbah <abdowahbah@gmail.com>
2025-11-02 16:53:48 +0200

    patient & dept tables

--- Back-end/Sehaty.Soluation/Sehaty.Core/Entities/Business_Entities/Department.cs
+++ Back-end/Sehaty.Soluation/Sehaty.Core/Entities/Business_Entities/Department.cs
@@ -9,5 +9,8 @@ namespace Sehaty.Core.Entites
     public class Department : BaseEntity
     {
         public string Name { get; set; }
+        public string NameLocal { get; set; }
+        public string Description { get; set; }
+        public List<Doctor> Doctors { get; set; }
     }
 }
--- Back-end/Sehaty.Soluation/Sehaty.Core/Entities/Business_Entities/Patient.cs
+++ Back-end/Sehaty.Soluation/Sehaty.Core/Entities/Business_Entities/Patient.cs
@@ -1,13 +1,45 @@
-﻿using System;
+﻿using Sehaty.Core.Entities.Business_Entities;
+using Sehaty.Core.Entities.User_Entities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Sehaty.Core.Entites
 {
+    public enum PatientStatus
+    {
+        Active,
+        Discharged,
+        Readmitted
+    }
     public class Patient : BaseEntity
     {
         public string Name { get; set; }
+        public string MRN { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string NationalId { get; set; }
+        public string BloodType { get; set; }
+        public string Allergies { get; set; }
+        public string ChrinicConditions { get; set; }
+        public string Address { get; set; }
+        public string EmergencyContactName { get; set; }
+        public string EmergencyContactPhone { get; set; }
+        public string Status { get; set; }
+        public DateTime RegistrationDate { get; set; }
+
+        // NAVIGATION PROPERTIES
+
+        [ForeignKey("ApplicationUser")]
+        public int UserId { get; set; }
+        public ApplicationUser User { get; set; }
+        public List<Appointment> Appointments { get; set; }
+        public List<Prescription> Prescriptions { get; set; }
+        public List<Billing> Billings { get; set; }
     }
 }
