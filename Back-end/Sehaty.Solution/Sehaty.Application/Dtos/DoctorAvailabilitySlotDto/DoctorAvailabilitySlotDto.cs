111c2884c87c6c46ed4191340ae6b2327cf951b0
Ahmed Abdulrahim <hemo089@outlook.com>
2025-11-06 22:46:41 +0200

    Impmemnt Crud For DoctorAvailabilitySlot Table && Add GetByName in MedicalRecord

---
+++ Back-end/Sehaty.Solution/Sehaty.Application/Dtos/DoctorAvailabilitySlotDto/DoctorAvailabilitySlotDto.cs
@@ -0,0 +1,21 @@
+﻿using Sehaty.Core.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sehaty.Application.Dtos.DoctorAvailabilitySlotDto
+{
+    public class DoctorAvailabilitySlotDto
+    {
+        public int Id { get; set; }
+        public int DoctorId { get; set; }
+        public String DayOfWeek { get; set; }
+        public TimeOnly StartTime { get; set; }
+        public TimeOnly EndTime { get; set; }
+        public bool IsRecurring { get; set; }
+        public DateOnly Date { get; set; }
+        public bool AvailableFlag { get; set; }
+    }
+}
