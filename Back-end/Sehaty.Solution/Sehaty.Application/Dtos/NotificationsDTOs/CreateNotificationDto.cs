c47b3f0f3fbd33ac3b5ad93246c577c1e31fdbf5
hanaa-elagmi <hanaa.elagmi@gmail.com>
2025-11-07 02:17:08 +0200

    Add Notifications Endpoints GetAll,GetById,Post

---
+++ Back-end/Sehaty.Solution/Sehaty.Application/Dtos/NotificationsDTOs/CreateNotificationDto.cs
@@ -0,0 +1,41 @@
+﻿using Sehaty.Core.Entites;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sehaty.Application.Dtos.NotificationsDTOs
+{
+    public class CreateNotificationDto
+    {
+        [Required]
+        [MaxLength(255)]
+        public string Title { get; set; }
+        [Required]
+        public string Message { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        [Required]
+        public DateTime? ReadAt { get; set; }
+        [Required]
+        public bool IsRead { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public NotificationPriority Priority { get; set; }
+        public NotificationType? NotificationType { get; set; }
+        [Required]
+        [MaxLength(10)]
+        public string RelatedEntityType { get; set; }
+        [Required]
+        [MaxLength(10)]
+        public int? RelatedEntityId { get; set; }
+        [Required]
+        public bool SentViaEmail { get; set; }
+        [Required]
+        public bool SentViaSMS { get; set; }
+        public int UserId { get; set; }
+        
+    }
+}
