e185a92bbde778c2e4e185ba88e5efebd55cb423
ahmedsobhy1998 <ahmedalgendy.dev@gmail.com>
2025-11-07 14:37:34 +0200

    Applied some modifications on the GetAll, GetById on the Appointment Controller

---
+++ Back-end/Sehaty.Solution/Sehaty.Application/Dtos/AppointmentDTOs/AppointmentReadDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sehaty.Application.Dtos.AppointmentDTOs
+{
+    public class AppointmentReadDto
+    {
+        public int Id { get; set; }
+        public DateTime AppointmentDateTime { get; set; }
+        public string ReasonForVisit { get; set; }
+        public string Status { get; set; }
+
+        // Nested related info
+        public string DoctorName { get; set; }
+        public string PatientName { get; set; }
+    }
+}
--- Back-end/Sehaty.Solution/Sehaty.Core/UnitOfWork.Contract/IUnitOfWork.cs
+++ Back-end/Sehaty.Solution/Sehaty.Core/UnitOfWork.Contract/IUnitOfWork.cs
@@ -5,6 +5,7 @@ namespace Sehaty.Core.UnitOfWork.Contract
 {
     public interface IUnitOfWork : IDisposable
     {
+
         IRepository<T> Repository<T>() where T : BaseEntity; // This Function To Create And Get The Repositry U Need When You Ask
         Task<int> CommitAsync(); // To Save All Changes At Once
     }
