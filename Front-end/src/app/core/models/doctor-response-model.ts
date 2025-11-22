d70b392f8b3a92a0df5c23d50915d5a47790789a
ibrahim <ibrahimsabaa244@gmail.com>
2025-11-22 21:46:31 +0200

    ibrahim

---
+++ Front-end/src/app/core/models/doctor-response-model.ts
@@ -0,0 +1,15 @@
+export interface Doctor {
+  id: number;
+  firstName: string;
+  lastName: string;
+  specialty: string;
+  licenseNumber: string;
+  qualifications: string;
+  yearsOfExperience: number;
+  profilePhotoUrl: string;
+  availabilityNotes: string;
+  userId: number;
+  user: string;
+  departmentId: number;
+  department: string;
+}
