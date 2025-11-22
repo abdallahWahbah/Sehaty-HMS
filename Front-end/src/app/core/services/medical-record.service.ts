d9dfeec47fa1af0b2fd3445965b579af17235fa3
Abdallah Wahbah <abdowahbah@gmail.com>
2025-11-22 20:21:52 +0200

    (admin medical records) itegration

---
+++ Front-end/src/app/core/services/medical-record.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MedicalRecordModel } from '../models/medical-record-model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MedicalRecordService {
+
+  private baseUrl = 'https://localhost:7086/api/MedicalRecords';
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<MedicalRecordModel[]> {
+    return this.http.get<MedicalRecordModel[]>(this.baseUrl);
+  }
+  
+}
