07e86d314539b36be7d962561bf66cdeca516b58
Ahmed-Abdulrahim <hemo089@outlook.com>
2025-12-01 21:34:29 +0200

    Add AI_powered appointment booking based on symptoms analysis

---
+++ Back-end/Sehaty.Solution/Sehaty.Application/Dtos/OpenAIDto/SuggestAppointmentBySymptomsDto/SuggestedSlotDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Sehaty.Application.Dtos.OpenAIDto.SuggestAppointmentBySymptomsDto
+{
+    public class SuggestedSlotDto
+    {
+        public int SlotId { get; set; }
+        public int DoctorId { get; set; }
+        public string DoctorName { get; set; }
+        public string Specialization { get; set; }
+        public DateOnly Date { get; set; }
+        public TimeOnly StartTime { get; set; }
+        public TimeOnly EndTime { get; set; }
+        public decimal ConsultationFee { get; set; }
+    }
+}
---
+++ Back-end/Sehaty.Solution/Sehaty.Application/Dtos/OpenAIDto/SuggestAppointmentBySymptomsDto/SymptomsAnalysisRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Sehaty.Application.Dtos.OpenAIDto.SuggestAppointmentBySymptomsDto
+{
+    public class SymptomsAnalysisRequestDto
+    {
+        public int PatientId { get; set; }
+        public string Symptoms { get; set; }
+    }
+}
