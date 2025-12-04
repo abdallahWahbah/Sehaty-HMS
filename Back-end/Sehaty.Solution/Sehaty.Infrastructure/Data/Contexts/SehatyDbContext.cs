cf7a12a041c416c62993bc8af9dc3ddd5989f456
Abdullah Muhammad Mahmoud <abdu.allah15@yahoo.com>
2025-12-04 20:41:48 +0200

    Edit Database New Real Data

--- Back-end/Sehaty.Solution/Sehaty.Infrastructure/Data/Contexts/SehatyDbContext.cs
+++ Back-end/Sehaty.Solution/Sehaty.Infrastructure/Data/Contexts/SehatyDbContext.cs
@@ -3,7 +3,7 @@
 namespace Sehaty.Infrastructure.Data.Contexts
 {
     // It Will Be Modefied To Inherit From IdentityDbContext<ApplicationUser,IdentityRole> To Add Auth Tables 
-    public class SehatyDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, int>
+    public class SehatyDbContext : IdentityDbContext<ApplicationUser,ApplicationRole,int>
     {
         // Main Constructor To Enable Dependancy Injection
         public SehatyDbContext(DbContextOptions<SehatyDbContext> options) : base(options) { }
@@ -24,7 +24,7 @@ namespace Sehaty.Infrastructure.Data.Contexts
         public DbSet<RefreshToken> RefreshTokens { get; set; }
         public DbSet<AuditLog> AuditLogs { get; set; }
         public DbSet<PasswordResetCode> PasswordResetCodes { get; set; }
-        public DbSet<Medication> Medications { get; set; }
+        //public DbSet<Medication> Medications { get; set; }
         public DbSet<PrescriptionMedications> PrescriptionMedications { get; set; }
         public DbSet<AppointmentAuditLog> AppointmentAuditLogs { get; set; }
         public DbSet<MedicalRecordAuditLog> MedicalRecordAuditLogs { get; set; }
@@ -35,67 +35,53 @@ namespace Sehaty.Infrastructure.Data.Contexts
         {
             base.OnModelCreating(modelBuilder);
 
-            //modelBuilder.Ignore<IdentityUserRole<int>>();
-
-            // --------------------------
-            // Custom User -> Role one-to-many
-            // --------------------------
-            //modelBuilder.Entity<ApplicationUser>()
-            //    .HasOne(u => u.Role)
-            //    .WithMany(r => r.Users)
-            //    .HasForeignKey(u => u.RoleId)
-            //    .OnDelete(DeleteBehavior.Restrict); // كل يوزر له رول واحد فقط
-
-            // --------------------------
-            // Apply your configurations from separate configuration classes
-            // --------------------------
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
             modelBuilder.ApplyConfiguration(new RefreshTokenConfiguration());
 
-            var hasher = new PasswordHasher<ApplicationUser>();
+            //var hasher = new PasswordHasher<ApplicationUser>();
 
-            var anonymousUser = new ApplicationUser()
-            {
-                Id = 999999,
-                CreatedAt = DateTime.MinValue,
-                Email = "Anonymous@example.com",
-                FirstName = "Anonymous",
-                LastName = "Anonymous",
-                IsActive = true,
-                PhoneNumber = "+2000000000000",
-                NormalizedEmail = "Anonymous@example.com".ToUpper(),
-                LanguagePreference = LanguagePreferenceEnum.Arabic,
-                UserName = "Anonymous",
-                NormalizedUserName = "Anonymous".ToUpper(),
-            };
-            anonymousUser.PasswordHash = hasher.HashPassword(anonymousUser, "P@ssw0rd");
+            //var anonymousUser = new ApplicationUser()
+            //{
+            //    Id = 999999,
+            //    CreatedAt = DateTime.MinValue,
+            //    Email = "Anonymous@example.com",
+            //    FirstName = "Anonymous",
+            //    LastName = "Anonymous",
+            //    IsActive = true,
+            //    PhoneNumber = "+2000000000000",
+            //    NormalizedEmail = "Anonymous@example.com".ToUpper(),
+            //    LanguagePreference = LanguagePreferenceEnum.Arabic,
+            //    UserName = "Anonymous",
+            //    NormalizedUserName = "Anonymous".ToUpper(),
+            //};
+            //anonymousUser.PasswordHash = hasher.HashPassword(anonymousUser,"P@ssw0rd");
 
-            modelBuilder.Entity<ApplicationUser>().HasData(anonymousUser);
+            //modelBuilder.Entity<ApplicationUser>().HasData(anonymousUser);
 
 
-            modelBuilder.Entity<Patient>().HasData(new Patient()
-            {
-                Id = 999999,
-                Address = "Unknown",
-                Allergies = "Unknown",
-                BloodType = "Unk",
-                ChrinicConditions = "Unknown",
-                DateOfBirth = DateTime.MinValue,
-                EmergencyContactName = "Unknown",
-                EmergencyContactPhone = "Unknown",
-                FirstName = "Anonymous",
-                LastName = "Unknown",
-                IsDeleted = false,
-                NationalId = "Unknown",
-                Patient_Id = "PT-2025-0000",
-                Status = PatientStatus.Active,
-                RegistrationDate = DateTime.MinValue,
-                Gender = "Unknown",
-                UserId = 999999
-            });
+            //modelBuilder.Entity<Patient>().HasData(new Patient()
+            //{
+            //    Id = 999999,
+            //    Address = "Unknown",
+            //    Allergies = "Unknown",
+            //    BloodType = "Unk",
+            //    ChrinicConditions = "Unknown",
+            //    DateOfBirth = DateTime.MinValue,
+            //    EmergencyContactName = "Unknown",
+            //    EmergencyContactPhone = "Unknown",
+            //    FirstName = "Anonymous",
+            //    LastName = "Unknown",
+            //    IsDeleted = false,
+            //    NationalId = "Unknown",
+            //    Patient_Id = "PT-2025-0000",
+            //    Status = PatientStatus.Active,
+            //    RegistrationDate = DateTime.MinValue,
+            //    Gender = "Unknown",
+            //    UserId = 999999
+            //});
 
-            modelBuilder.Entity<IdentityUserRole<int>>().HasData(new IdentityUserRole<int>() { UserId = 999999, RoleId = 3 });
+            //modelBuilder.Entity<IdentityUserRole<int>>().HasData(new IdentityUserRole<int>() { UserId = 999999,RoleId = 3 });
 
         }
     }
