fcca3093799ebcfe595c5fe8e9eae545b1ce52df
Ahmed-Abdulrahim <hemo089@outlook.com>
2025-12-05 22:10:35 +0200

    Fix Payment Response

--- Back-end/Sehaty.Solution/Sehaty.APIs/Controllers/BillingController.cs
+++ Back-end/Sehaty.Solution/Sehaty.APIs/Controllers/BillingController.cs
@@ -1,6 +1,6 @@
 ﻿namespace Sehaty.APIs.Controllers
 {
-    public class BillingController(IUnitOfWork unit,IMapper mapper) : ApiBaseController
+    public class BillingController(IUnitOfWork unit, IMapper mapper) : ApiBaseController
     {
         [HttpGet("GetAllForPatient")]
         [Authorize(Roles = "Patient")]
@@ -12,25 +12,25 @@
                 var patient = await unit.Repository<Patient>()
                     .GetFirstOrDefaultAsync(P => P.UserId == patientUserId);
 
-                if(patient == null)
-                    return NotFound(new ApiResponse(404,"Patient not found"));
+                if (patient == null)
+                    return NotFound(new ApiResponse(404, "Patient not found"));
 
-                if(patient.Id != id)
-                    return BadRequest(new ApiResponse(400,"you don't have access to This Billing"));
+                if (patient.Id != id)
+                    return BadRequest(new ApiResponse(400, "you don't have access to This Billing"));
 
                 var spec = new BillingSpec(b => b.PatientId == patient.Id && b.Status != BillingStatus.Pending);
                 var allBillings = await unit.Repository<Billing>()
                     .GetAllWithSpecAsync(spec);
 
-                if(!allBillings.Any())
-                    return NotFound(new ApiResponse(404,"No billing history found for patient"));
+                if (!allBillings.Any())
+                    return NotFound(new ApiResponse(404, "No billing history found for patient"));
 
                 var data = mapper.Map<List<BillingReadDto>>(allBillings);
                 return Ok(data);
             }
-            catch(FormatException)
+            catch (FormatException)
             {
-                return BadRequest(new ApiResponse(400,"Invalid user Authentication "));
+                return BadRequest(new ApiResponse(400, "Invalid user Authentication "));
             }
         }
         [HttpGet]
@@ -41,8 +41,8 @@
             var allBillings = await unit.Repository<Billing>()
                 .GetAllWithSpecAsync(spec);
 
-            if(!allBillings.Any())
-                return NotFound(new ApiResponse(404,"No billing found."));
+            if (!allBillings.Any())
+                return NotFound(new ApiResponse(404, "No billing found."));
 
             var data = mapper.Map<List<BillingReadDto>>(allBillings);
             return Ok(data);
